131256e97f99670315517ace9bbb8d880f2bb698
ezedksl <ezedksl@gmail.com>
2020-10-26 23:03:47 +0000

    update

--- printf.c
+++ printf.c
@@ -9,6 +9,7 @@ int _printf(const char *format, ...)
 	int charCount = 0;
 	char temp = '%';
 	va_list list;
+	unsigned int charSize = sizeof(char);
 
 	if (format == NULL || *format == 0)
 		return (-1);
@@ -17,14 +18,14 @@ int _printf(const char *format, ...)
 	{
 		if (*format != temp)
 		{
-			write(1, format, 1);
+			write(1, format, charSize);
 			charCount++;
 		}
 		else
 		{
 			if (*(format + 1) == 0)
 			{
-				write(1, &temp, 1);
+				write(1, &temp, charSize);
 				return (-1);
 			}
 			else if (*(format + 1) == 'c'
@@ -36,7 +37,7 @@ int _printf(const char *format, ...)
 			}
 			else
 			{
-				write(1, format, 1);
+				write(1, format, charSize);
 				charCount++;
 			}
 		}
@@ -53,13 +54,13 @@ int _printf(const char *format, ...)
   */
 int suich(const char *format, va_list list)
 {
-	int charCount = 0, c, argLen = 0;
+	int charCount = 0, c, argLen = 0, charSize = sizeof(char);
 	char *s, temp = '%';
 
 	switch (*format)
 	{
 		case '%':
-			write(1, &temp, 1);
+			write(1, &temp, charSize);
 			charCount++;
 			break;
 		case 'c':
@@ -68,7 +69,7 @@ int suich(const char *format, va_list list)
 			{
 				return (0);
 			}
-			write(1, &c, 1);
+			write(1, &c, charSize);
 			charCount++;
 			break;
 		case 's':
@@ -76,7 +77,7 @@ int suich(const char *format, va_list list)
 			if (s == NULL)
 				s = "(null)";
 			for (argLen = 0; *s != 0; argLen++, s++)
-				write(1, s, 1);
+				write(1, s, charSize);
 			charCount += (argLen);
 			break;
 		default:
