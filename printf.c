d763897972079ec8e1ea3fa55e84a2862757488f
mi-kei-la <micaela.percovich@gmail.com>
2020-10-26 01:08:50 +0000

    update

--- printf.c
+++ printf.c
@@ -3,12 +3,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "holberton.h"
-
+/**
+  * _printf - print efe
+  * @format: string to print
+  * Return: int
+  */
 int _printf(const char *format, ...)
 {
-	int charCount = 0, argLen = 0;
-	char c, temp = '%';
-	char *s;
+	int charCount = 0, i = 0;
+	char temp = '%';
 	va_list list;
 
 	va_start(list, format);
@@ -17,36 +20,48 @@ int _printf(const char *format, ...)
 		if (*format != temp && format)
 		{
 			write(1, format, 1);
-			printf("char");
 			charCount++;
 		}
 		else
 		{
 			format++;
-			switch(*format)
-			{
-				case '%':
-					write(1, &temp, 1);
-					printf("case1");
-					charCount++;
-					break;
-				case 'c':
-					c = va_arg(list, int);
-					write(1, &c, 1);
-					printf("case2");
-					charCount++;
-					break;
-				case 's':
-					s = va_arg(list, char*);
-					for(argLen = 0; *s != 0; argLen++, s++)
-					{
-					write(1, s, 1);
-					}
-					charCount += argLen;
-					break;
-			}
+			suich(format, list);
+			charCount += i;
 		}
 		format++;
 	}
 	return (charCount);
 }
+/**
+  * suich - switch for conversion specifiers
+  * @format: string to print
+  * @list: arguments to print
+  * Return: count of printed chars
+  */
+int suich(const char *format, va_list list)
+{
+	int charCount = 0, c, argLen = 0;
+	char *s, temp = '%';
+
+	switch (*format)
+	{
+		case '%':
+			write(1, &temp, 1);
+			printf("case1");
+			charCount++;
+			break;
+		case 'c':
+			c = va_arg(list, int);
+			write(1, &c, 1);
+			charCount++;
+			break;
+		case 's':
+			s = va_arg(list, char*);
+			for (argLen = 0; *s != 0; argLen++, s++)
+				write(1, s, 1);
+			charCount += argLen;
+			break;
+	}
+	return (charCount);
+}
+
